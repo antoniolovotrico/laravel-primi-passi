a516896748010dc8f767413ed55542bb5cdb321b
antoniolovotrico <antonio.lovotrico@icloud.com>
2021-02-11 02:54:07 +0100

    update css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,11 +2,10 @@
 
 @section('main_content')
 <h1>{{$title}}</h1>
-<div class="top_ruolo">
+<div class="top_ruolo d_flex">
     @if($inter)
         @foreach($inter as $player)
-            <h3>Nome : {{ $player['name'] }}</h3>
-            <h3>Ruolo : {{ $player['position'] }}</h3>
+            <h3>Nome : {{ $player['name'] }}  ||  Ruolo : {{ $player['position'] }}</h3>
         @endforeach
     @else
         <h3>Nothing to show</h3>
